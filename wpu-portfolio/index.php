a6a3a4e5c41eab9a60ff74e4e5ae9f2b8675064f
siskoala <siskoala.drive@gmail.com>
2026-04-24 01:28:17 +0700

    updated

---
+++ wpu-portfolio/index.php
@@ -0,0 +1,327 @@
+<?php 
+
+  function get_CURL($url)
+  {
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $result = curl_exec($curl);
+    curl_close($curl);
+    return json_decode($result, true);
+    // var_dump($result);
+  }
+
+  $result = get_CURL('https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&forHandle=@sandhikagalihWPU&key=XXXX');
+
+  $youtubeProfilePic = $result['items'][0]['snippet']['thumbnails']['medium']['url'];
+  $youtubeChannel    = $result['items'][0]['snippet']['title'];
+  $youtubeSubscribe  = $result['items'][0]['statistics']['subscriberCount'];
+  $formattedSub      = number_format($youtubeSubscribe, 0, ',', '.');
+
+  // latest video 
+  $urlLatest = 'https://www.googleapis.com/youtube/v3/search?key=XXXX&channelId=XXXX&maxResult=1&order=date&part=snippet';
+  $result = get_CURL($urlLatest);
+  $latestVideoId = $result['items'][0]['id']['videoId'];
+  // var_dump($result);
+
+  // Instagram API
+  $result = get_CURL('https://graph.instagram.com/me?fields=user_id,username,profile_picture_url,followers_count&access_token=XXXX');
+  $igProfilePic = $result['profile_picture_url'];
+  $username = $result['username'];
+  $followers = $result['followers_count'];
+?>
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+    <!-- My CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>My Portfolio</title>
+  </head>
+  <body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#home">Sandhika Galih</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#home">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#portfolio">Portfolio</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+
+    <div class="jumbotron" id="home">
+      <div class="container">
+        <div class="text-center">
+          <img src="img/profile1.png" class="rounded-circle img-thumbnail">
+          <h1 class="display-4">Sandhika Galih</h1>
+          <h3 class="lead">Lecturer | Programmer | Youtuber</h3>
+        </div>
+      </div>
+    </div>
+
+
+    <!-- About -->
+    <section class="about" id="about">
+      <div class="container">
+        <div class="row mb-4">
+          <div class="col text-center">
+            <h2>About</h2>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-md-5">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus, molestiae sunt doloribus error ullam expedita cumque blanditiis quas vero, qui, consectetur modi possimus. Consequuntur optio ad quae possimus, debitis earum.</p>
+          </div>
+          <div class="col-md-5">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus, molestiae sunt doloribus error ullam expedita cumque blanditiis quas vero, qui, consectetur modi possimus. Consequuntur optio ad quae possimus, debitis earum.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Youtube & IG -->
+    <section class="social bg-light" id="social">
+      <div class="container">
+        <div class="row pt-4 mb-4">
+          <div class="col text-center">
+            <h2>Social Media</h2>
+          </div>
+        </div>
+
+        <div class="row justify-content-center">
+          <div class="col-md-5">
+            <div class="row">
+              <div class="col-md-4">
+                <img src="<?= $youtubeProfilePic; ?>" alt="image" width="120" class="rounded-circle shadow img-thumbnail">
+              </div>
+              <div class="col-md-8">
+                <h5><?= $youtubeChannel; ?></h5>
+                <p><?= $formattedSub; ?> Subscribers</p>
+                <div class="g-ytsubscribe" data-channelid="UCkXmLjEr95LVtGuIm3l2dPg" data-layout="default" data-count="default"></div>
+              </div>
+            </div>
+
+            <div class="row mt-3 pb-3">
+              <div class="col">
+                <div class="ratio ratio-16x9">
+                  <iframe src="https://www.youtube.com/embed/<?= $latestVideoId; ?>" title="YouTube video" allowfullscreen></iframe>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-5">
+            <div class="row">
+              <div class="col-md-4">
+                <img src="<?= $igProfilePic; ?>" alt="image" width="120" class="rounded-circle shadow img-thumbnail">
+              </div>
+              <div class="col-md-8">
+                <h5>@<?= $username; ?></h5>
+                <p><?= $followers; ?> Followers</p>
+              </div>
+            </div>
+
+            <div class="row mt-3 pb-3">
+              <div class="col">
+                <div class="ig-thumbnail">
+                  <img src="img/thumbs/1.png" alt="">
+                </div>
+              </div>
+              <div class="col">
+                <div class="ig-thumbnail">
+                  <img src="img/thumbs/2.png" alt="">
+                </div>
+              </div>
+              <div class="col">
+                <div class="ig-thumbnail">
+                  <img src="img/thumbs/3.png" alt="">
+                </div>
+              </div>
+              <div class="col">
+                <div class="ig-thumbnail">
+                  <img src="img/thumbs/4.png" alt="">
+                </div>
+              </div>
+              <div class="col">
+                <div class="ig-thumbnail">
+                  <img src="img/thumbs/5.png" alt="">
+                </div>
+              </div>
+              <div class="col">
+                <div class="ig-thumbnail">
+                  <img src="img/thumbs/6.png" alt="">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+    <!-- Portfolio -->
+    <section class="portfolio" id="portfolio">
+      <div class="container">
+        <div class="row pt-4 mb-4">
+          <div class="col text-center">
+            <h2>Portfolio</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md mb-4">
+            <div class="card">
+              <img class="card-img-top" src="img/thumbs/1.png" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md mb-4">
+            <div class="card">
+              <img class="card-img-top" src="img/thumbs/2.png" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md mb-4">
+            <div class="card">
+              <img class="card-img-top" src="img/thumbs/3.png" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>   
+        </div>
+
+        <div class="row">
+          <div class="col-md mb-4">
+            <div class="card">
+              <img class="card-img-top" src="img/thumbs/4.png" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div> 
+          <div class="col-md mb-4">
+            <div class="card">
+              <img class="card-img-top" src="img/thumbs/5.png" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md mb-4">
+            <div class="card">
+              <img class="card-img-top" src="img/thumbs/6.png" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <!-- Contact -->
+    <section class="contact bg-light" id="contact">
+      <div class="container mb-5">
+        <div class="row pt-4 mb-4">
+          <div class="col text-center">
+            <h2>Contact</h2>
+          </div>
+        </div>
+
+        <div class="row justify-content-center">
+          <div class="col-lg-4">
+            <div class="card bg-primary text-white mb-4 text-center">
+              <div class="card-body">
+                <h5 class="card-title">Contact Me</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+            
+            <ul class="list-group mb-4">
+              <li class="list-group-item"><h3>Location</h3></li>
+              <li class="list-group-item">My Office</li>
+              <li class="list-group-item">Jl. Setiabudhi No. 193, Bandung</li>
+              <li class="list-group-item">West Java, Indonesia</li>
+            </ul>
+          </div>
+
+          <div class="col-lg-6">
+            
+            <form>
+              <div class="form-group">
+                <label for="nama">Nama</label>
+                <input type="text" class="form-control" id="nama">
+              </div>
+              <div class="form-group">
+                <label for="email">Email</label>
+                <input type="text" class="form-control" id="email">
+              </div>
+              <div class="form-group">
+                <label for="phone">Phone Number</label>
+                <input type="text" class="form-control" id="phone">
+              </div>
+              <div class="form-group">
+                <label for="message">Message</label>
+                <textarea class="form-control" id="message" rows="3"></textarea>
+              </div>
+              <div class="form-group">
+                <button type="button" class="btn btn-primary">Send Message</button>
+              </div>
+            </form>
+
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <!-- footer -->
+    <footer class="bg-dark text-white">
+      <div class="container">
+        <div class="row">
+          <div class="col text-center">
+            <p>Copyright &copy; 2018.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-4.0.0.js" integrity="sha256-9fsHeVnKBvqh3FB2HYu7g2xseAZ5MlN6Kz/qnkASV8U=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <script src="https://apis.google.com/js/platform.js"></script>
+  </body>
+</html>
